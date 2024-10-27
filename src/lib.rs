a80cd32d6d4f8a679e68c2453ea60d07526d6980
Drew Walton <drewwalton19216801@gmail.com>
2024-10-26 19:31:15 -0600

    Require docs

--- src/lib.rs
+++ src/lib.rs
@@ -3,10 +3,12 @@
 //! This crate provides a 6502 CPU emulator that can be used to run 6502 machine
 //! code. It provides a simple API for executing instructions and accessing the
 //! CPU's registers and memory.
+
+#![warn(missing_docs)]
+pub mod addressing_modes;
 pub mod bus;
 pub mod cpu;
 pub mod instructions;
-pub mod addressing_modes;
 pub mod registers;
 
 #[cfg(test)]
